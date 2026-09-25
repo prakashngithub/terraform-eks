588ad2b60346f0153abaf1e679a34960d9915e71
Prakash <jpclouddev12.com>
2026-09-25 23:29:57 +1000

    update3 eks

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -16,7 +16,7 @@ module "eks" {
   eks_managed_node_groups = {
     ami_type               = "AL2023_x86_64_STANDARD"
     instance_types         = ["t2.xlarge"]
-    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+    vpc_security_group_ids = aws_security_group.all_worker_mgmt.id
     node_group = {
       min_size     = 2
       max_size     = 6
