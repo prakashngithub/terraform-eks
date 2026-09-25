fb0cce79a46c004c3cb5942bfd60d18ff20c2de0
Prakash <jpclouddev12.com>
2026-09-25 23:16:26 +1000

    update eks

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,8 +1,8 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "21.0"
-  cluster_name    = local.cluster_name
-  cluster_version = var.kubernetes_version
+  name    = local.cluster_name
+  kubernetes_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
 
   enable_irsa = true
@@ -13,7 +13,7 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
-  eks_managed_node_group_defaults = {
+  eks_managed_node_groups = {
     ami_type               = "AL2023_x86_64_STANDARD"
     instance_types         = ["t2.xlarge"]
     vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
